d66cca90230eb2b5dae90792c5f3e7d7cd690d19
Judrey Padsuyan <2232249@slu.edu.ph>
2025-03-17 09:24:56 +0800

    Finalized the Naive Bayes Model

--- naive-bayes/nb-training-judrey.py
+++ naive-bayes/nb-training-judrey.py
@@ -4,7 +4,8 @@ from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.preprocessing import LabelEncoder
 from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import classification_report, accuracy_score, confusion_matrix, roc_auc_score
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix, roc_auc_score, recall_score, \
+    precision_score
 import seaborn as sns
 
 # Load the dataset
@@ -55,28 +56,62 @@ y_pred_proba = model.predict_proba(X_test)
 
 # Evaluate the model's performance
 accuracy = accuracy_score(y_test, y_pred)
-conf_matrix = confusion_matrix(y_test, y_pred)
+test_conf_matrix = confusion_matrix(y_test, y_pred)
 class_report = classification_report(y_test, y_pred, target_names=target_encoder.classes_)
-roc_aoc= roc_auc_score(y_test, y_pred_proba, multi_class='ovr')
+roc_auc= roc_auc_score(y_test, y_pred_proba, multi_class='ovr')
 
 # Visualize the confusion matrix
 plt.figure(figsize=(8, 6))
-sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues',
+sns.heatmap(test_conf_matrix, annot=True, fmt='d', cmap='Blues',
             xticklabels=target_encoder.classes_,
             yticklabels=target_encoder.classes_)
 plt.xlabel('Predicted')
 plt.ylabel('Actual')
-plt.title('Confusion Matrix')
+plt.title('Confusion Matrix (Using Test Data)')
 plt.tight_layout()
 plt.savefig('confusion_matrix.png')
 plt.show()
 
+# Print the Results
 print(f"Accuracy: {accuracy}")
-print("Confusion Matrix:")
-print(conf_matrix)
 print("Classification Report:")
 print(class_report)
-print("ROC-AUC:")
-print(roc_aoc)
-print("Mean Cross-Validation Accuracy:", cv_scores.mean())
+
+print("Mean Cross-Validation Accuracy (Using Test Data):", cv_scores.mean())
+print("Confusion Matrix (Using Test Data):")
+print(test_conf_matrix)
+# Accuracy
+test_accuracy = accuracy_score(y_test, y_pred)
+print("Test Accuracy:", test_accuracy)
+# Precision (macro)
+test_precision = precision_score(y_test, y_pred, average='macro')
+print("Test Precision (macro):", test_precision)
+# Recall (macro)
+test_recall = recall_score(y_test, y_pred, average='macro')
+print(f"Test Recall (macro): {test_recall}")
+# ROC-AUC (macro)
+print(f"Test ROC-AUC (macro): {roc_auc}")
+
+
+# Predict on unseen data
+unseen_pred = model.predict(X_unseen)
+unseen_accuracy = accuracy_score(y_unseen, unseen_pred)
+unseen_pred_proba = model.predict_proba(X_unseen)
+unseen_conf_matrix = confusion_matrix(y_unseen, unseen_pred)
+unseen_recall = recall_score(y_unseen, unseen_pred, average='macro')
+unseen_precision = precision_score(y_unseen, unseen_pred, average='macro')
+unseen_class_report = classification_report(y_unseen, unseen_pred, target_names=target_encoder.classes_)
+unseen_roc_auc= roc_auc_score(y_unseen, unseen_pred_proba, multi_class='ovr')
+
+print("Confusion Matrix (Using Unseen Data):")
+print(unseen_conf_matrix)
+# Accuracy
+print("Unseen Data Accuracy:", unseen_accuracy)
+# Precision (macro)
+print("Unseen Data Precision (macro):", test_precision)
+# Recall (macro)
+print(f"Unseen Data Recall (macro): {test_recall}")
+# ROC-AUC (macro)
+print(f"Unseen Data ROC-AUC (macro): {unseen_roc_auc}")
+
 
